4007d5b5570815a4f2f76c1d6a82b38bb41f088c
Kelly King <kelly.danielle.king@gmail.com>
2022-11-22 22:41:37 +0000

    lessons 8-9

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ const app = Vue.createApp({
     return {
       title: "Comedy events",
       tickets: 10,
+      cart: 0,
       image: "http://www.fillmurray.com/200/200",
       details: { Name: "Bill Murray", Age: 72, Location: "Chicago "}
     };
+  },
+  methods: {
+    updateCart() {
+      this.cart++;
+      this.tickets--;
+    }
   }
 });
