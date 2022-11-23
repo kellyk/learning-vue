58c8e4511f95b1ac8fb29e363a9574927458f023
Kelly King <kelly.danielle.king@gmail.com>
2022-11-23 10:39:07 +0000

    lessons 10-12

--- main.js
+++ main.js
@@ -4,14 +4,27 @@ const app = Vue.createApp({
       title: "Comedy events",
       tickets: 10,
       cart: 0,
+      ticketTypes: [
+        { id: 'bronzeTicket', name: 'Bronze', image: 'http://www.fillmurray.com/200/200', tickets: 3, description: 'Bronze ticket' },
+        { id: 'silverTicket', name: 'Silver', image: 'http://www.fillmurray.com/200/202', tickets: 10, description: 'Silver ticket' },
+        { id: 'goldTicket', name: 'Gold', image: 'http://www.fillmurray.com/200/199', tickets: 40, description: 'Gold ticket' }
+      ],
       image: "http://www.fillmurray.com/200/200",
       details: { Name: "Bill Murray", Age: 72, Location: "Chicago "}
-    };
+    }
   },
   methods: {
     updateCart() {
       this.cart++;
       this.tickets--;
+    },
+    updateImage(id) {
+      this.image = this.ticketTypes.filter(type=> type.id === id)[0].image;
+    }
+  },
+  computed: {
+    soldOut() {
+      return this.tickets === 0;
     }
   }
 });
