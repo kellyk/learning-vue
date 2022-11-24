cfef5462e14e69e5ae1c630384fdc1ae637027bf
Kelly King <kelly.danielle.king@gmail.com>
2022-11-24 22:29:58 +0000

    chapters 15-17

---
+++ components/TicketDetails.js
@@ -0,0 +1,31 @@
+app.component('ticket-details', {
+  props: {
+    name: String,
+    description: String,
+    tickets: Number,
+  },
+  template: `
+    <div>
+      <h4>{{ name }}</h4>
+      <div class="addTickets">
+        <select class="ticketSelector" v-model="ticketsToBuy">
+          <option value=0>0</option>
+          <option v-for="number in ticketsLeft" :value="number">{{number}}</option>
+        </select>
+        <button v-if="ticketsToBuy > 0" @click="addTickets(ticketsToBuy)">Add</button>
+      </div>
+    </div>
+  `,
+  data() {
+    return {
+      ticketsToBuy: 0,
+      ticketsLeft: this.tickets
+    }
+  },
+  methods: {
+    addTickets(num) {
+      this.ticketsLeft -=num;
+      this.ticketsToBuy = 0;
+    }
+  }
+})
--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 const app = Vue.createApp({
   data() {
     return {
-      message: '',
+      name: '',
       soldOutClass: 'soldOut',
       title: "Comedy events",
       tickets: 10,
       cart: 0,
       ticketTypes: [
-        { id: 'bronzeTicket', name: 'Bronze', image: 'http://www.fillmurray.com/200/200', tickets: 3, description: 'Bronze ticket' },
-        { id: 'silverTicket', name: 'Silver', image: 'http://www.fillmurray.com/200/202', tickets: 10, description: 'Silver ticket' },
-        { id: 'goldTicket', name: 'Gold', image: 'http://www.fillmurray.com/200/199', tickets: 40, description: 'Gold ticket' }
+        {
+          id: 'bronzeTicket',
+          name: 'Bronze',
+          image: 'http://www.fillmurray.com/200/200',
+          tickets: 3,
+          description: 'Bronze ticket'
+        },
+        {
+          id: 'silverTicket',
+          name: 'Silver',
+          image: 'http://www.fillmurray.com/200/202',
+          tickets: 10,
+          description: 'Silver ticket'
+        },
+        {
+          id: 'goldTicket',
+          name: 'Gold',
+          image: 'http://www.fillmurray.com/200/199',
+          tickets: 40,
+          description: 'Gold ticket'
+        }
       ],
       image: "http://www.fillmurray.com/200/200",
       details: { Name: "Bill Murray", Age: 72, Location: "Chicago "}
